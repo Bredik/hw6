a7b726948e4612a3fbfbd681e9d5253aba9de565
Bredik <br-bred@yandex.ru>
2024-07-28 18:52:47 +0300

    task 1 - done

--- task-1-delayed-addition-rejected-tasks/src/main/java/com/github/javarar/rejected/task/DelayedOnRejectedThreadExecutor.java
+++ task-1-delayed-addition-rejected-tasks/src/main/java/com/github/javarar/rejected/task/DelayedOnRejectedThreadExecutor.java
@@ -1,4 +1,53 @@
 package com.github.javarar.rejected.task;
 
+import lombok.extern.log4j.Log4j2;
+
+import java.util.concurrent.*;
+
+@Log4j2
 public class DelayedOnRejectedThreadExecutor {
+    private final ScheduledExecutorService scheduledExecutor;
+    private static final int rejectRetryDelay = 2;
+    private static final TimeUnit rejectRetryTimeUnit = TimeUnit.SECONDS;
+
+    private final int poolSize;
+    private final int queueSize;
+
+    public DelayedOnRejectedThreadExecutor(int poolSize, int queueSize) {
+        this.poolSize = poolSize;
+        this.queueSize = queueSize;
+        this.scheduledExecutor = Executors.newScheduledThreadPool(30);
+    }
+
+    public ExecutorService create() {
+        BlockingQueue<Runnable> queueMain = new ArrayBlockingQueue<>(queueSize);
+        return new ThreadPoolExecutor(
+                poolSize,
+                poolSize,
+                0,
+                TimeUnit.SECONDS,
+                queueMain,
+                new RepeaterPolicy());
+    }
+
+    public class RepeaterPolicy implements RejectedExecutionHandler {
+        public RepeaterPolicy() {
+        }
+
+        @Override
+        public void rejectedExecution(Runnable r, ThreadPoolExecutor executor) {
+            rejectedHandler(r, executor);
+        }
+    }
+
+    private void rejectedHandler(Runnable runnable, ThreadPoolExecutor threadPoolExecutor) {
+        log.error("Задача не поместилась: {}", Thread.currentThread().getName());
+        scheduledExecutor.schedule(() -> {
+                    System.out.println("Пробуем добавить задачу заново " + Thread.currentThread().getName());
+                    Executors.newSingleThreadExecutor().submit(runnable);
+                },
+                rejectRetryDelay,
+                rejectRetryTimeUnit);
+    }
+
 }
--- task-1-delayed-addition-rejected-tasks/src/test/java/com/github/javarar/rejected/task/DelayedOnRejectedThreadExecutorTest.java
+++ task-1-delayed-addition-rejected-tasks/src/test/java/com/github/javarar/rejected/task/DelayedOnRejectedThreadExecutorTest.java
@@ -1,6 +1,40 @@
 package com.github.javarar.rejected.task;
 
+import lombok.extern.log4j.Log4j2;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Future;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicLong;
+
+@Log4j2
 public class DelayedOnRejectedThreadExecutorTest {
+    @Test
+    void check() throws InterruptedException, ExecutionException {
+        ExecutorService exe = new DelayedOnRejectedThreadExecutor(2, 3).create();
 
+        int taskCount = 10;
+        var futures = new ArrayList<Future<Integer>>();
 
+        for (int i = 0; i < taskCount; i++) {
+            int finalI = i;
+            futures.add(exe.submit(() -> {
+                System.out.println("Я выполняюсь " + Thread.currentThread().getName());
+                var tmp = finalI;
+                return tmp++;
+            }));
+        }
+
+        exe.shutdown();
+        exe.awaitTermination(Long.MAX_VALUE, TimeUnit.NANOSECONDS);
+        Thread.sleep(5000);
+        Assertions.assertEquals(10, futures.size());
+        for (int i = 0; i < taskCount; i++) {
+            Assertions.assertEquals(i, futures.get(i).get());
+        }
+    }
 }
